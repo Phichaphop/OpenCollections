3a970474e5258bcb721adca472ca3914f916bee9
Phichaphop <Phichaphop.b@gmail.com>
2024-06-09 13:22:03 +0700

    Update config.js

--- resource/js/config.js
+++ resource/js/config.js
@@ -1,17 +1,24 @@
-function set() {
-    if (localStorage.getItem("theme") === "theme-dark") {
-      document.getElementById("theme-icon").innerHTML = "nightlight"
-    } else {
-      document.getElementById("theme-icon").innerHTML = "light_mode"
+  function set() {
+    const currentHour = new Date().getHours();
+    const themeElement = document.getElementById("theme");
+    
+    if (currentHour === 8 || localStorage.getItem("theme") === "theme-light") {
+        document.documentElement.setAttribute("data-theme", "light");
+        themeElement.innerHTML = "light_mode";
+        localStorage.setItem("theme", "theme-light");
+    } else if (currentHour === 18 || localStorage.getItem("theme") === "theme-dark") {
+        document.documentElement.setAttribute("data-theme", "dark");
+        themeElement.innerHTML = "nightlight";
+        localStorage.setItem("theme", "theme-dark");
     }
-  
+
     var lang = document.cookie.split('; ').find(row => row.startsWith('lang=')).split('=')[1];
     if (lang === "en") {
       document.getElementById("lang").innerHTML = "language_us";
     } else {
       document.getElementById("lang").innerHTML = "translate";
     }
-  }
+}
 
   function getCookie(name) {
     var value = `; ${document.cookie}`;
